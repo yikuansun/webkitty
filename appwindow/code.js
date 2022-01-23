7a83e99c123a4649c6ca299cfc8c9057aea60f23
yikuansun <yikuansun2015@gmail.com>
2022-01-23 14:26:22 -0500

    directory selector

---
+++ appwindow/code.js
@@ -0,0 +1,12 @@
+const { remote, app, shell } = require("electron");
+const { dialog } = remote;
+
+var projectdirectory = "";
+document.querySelector("#projectselect").addEventListener("click", function() {
+    var dir = dialog.showOpenDialogSync({
+        properties: ["openDirectory"]
+    });
+    if (dir) {
+        projectdirectory = dir;
+    }
+});
