b50e5a8bcf504bcef867d9b18345cb2ed125503d
yikuansun <yikuansun2015@gmail.com>
2022-01-23 14:41:06 -0500

    Set up editor

--- appwindow/code.js
+++ appwindow/code.js
@@ -1,13 +1,25 @@
 const { remote, app, shell } = require("electron");
+const fs = require("fs");
 const { dialog } = remote;
 
 var projectdirectory = "";
+function setProject(dir) {
+    var dircontents = fs.readdirSync(dir);
+    var fileselect = document.querySelector("#fileselect");
+    for (var file of dircontents) { // can't handle nested folder yet
+        var option = document.createElement("option");
+        option.innerText = file;
+        fileselect.appendChild(option);
+    }
+}
+
 document.querySelector("#projectselect").addEventListener("click", function() {
     var dir = dialog.showOpenDialogSync({
         properties: ["openDirectory"]
     });
-    if (dir) {
-        projectdirectory = dir;
+    if (dir[0]) {
+        projectdirectory = dir[0];
+        setProject(projectdirectory);
         document.querySelector("#landingscreen").style.display = "none";
     }
 });
